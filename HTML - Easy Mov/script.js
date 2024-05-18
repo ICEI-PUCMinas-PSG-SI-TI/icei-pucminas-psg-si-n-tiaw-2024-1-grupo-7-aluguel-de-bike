2c1f2b59f5faf6eb0adf6079dc36930747a6be1f
Andre Leandro <andrephilleandro@gmail.com>
2024-05-18 12:02:11 -0300

    ajuste do Arquivo Json para fomr de login

--- HTML - Easy Mov/script.js
+++ HTML - Easy Mov/script.js
@@ -1,36 +1,33 @@
 document.getElementById('loginButton').addEventListener('click', fazerLogin);
 
-function fazerLogin() {
+async function fazerLogin() {
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
-   
 
-    /* Adicionar mais credenciais aqui, juntamente no arquivo Json*/
-    var credenciais =  [
-        {
-            "email": "administrador@adm.com",
-            "senha": "Admin123"
+    try {
+        // Carregar credenciais do arquivo JSON
+        const response = await fetch('testelogin.json');
+        if (!response.ok) {
+            throw new Error('Erro ao carregar o arquivo JSON');
         }
-        ,
-    {
-        "email": "Pucminas@puc.br",
-        "senha": "Puc12345"
-    }
-    
-    ];
+        const credenciais = await response.json();
 
-    var loginValido = false;
-    for (var i = 0; i < credenciais.length; i++) {
-        if (credenciais[i].email === email && credenciais[i].senha === senha) {
-            loginValido = true;
-            break;
+        // Verificar as credenciais
+        var loginValido = false;
+        for (var i = 0; i < credenciais.length; i++) {
+            if (credenciais[i].email === email && credenciais[i].senha === senha) {
+                loginValido = true;
+                break;
+            }
         }
-    }
-    
-    if (loginValido) {
-        
-        window.location.href = '/HTML - Easy Mov/ADM/Logado.html';
-    } else {
-        alert('Email ou senha incorretos. Por favor, tente novamente.');
+
+        if (loginValido) {
+            window.location.href = '/HTML - Easy Mov/ADM/Logado.html';
+        } else {
+            alert('Email ou senha incorretos. Por favor, tente novamente.');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao tentar fazer login. Por favor, tente novamente mais tarde.');
     }
 }
