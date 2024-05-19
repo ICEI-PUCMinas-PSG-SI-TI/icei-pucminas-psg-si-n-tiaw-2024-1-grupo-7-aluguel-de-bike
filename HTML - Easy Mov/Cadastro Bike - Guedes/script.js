d6f78647573dc6b78abff8fb33ae6888f27cefad
Davi <davi.avila@sga.pucminas.br>
2024-05-19 19:29:59 -0300

    Java script e JSON para cadastro das Bikes

--- HTML - Easy Mov/Cadastro Bike - Guedes/script.js
+++ HTML - Easy Mov/Cadastro Bike - Guedes/script.js
@@ -1,34 +1,87 @@
+//cadastro de clientes
+
 document.getElementById('loginButton').addEventListener('click', fazerLogin);
 
-function fazerLogin() {
+async function fazerLogin() {
     var email = document.getElementById('email').value;
     var senha = document.getElementById('senha').value;
 
-    /* Adicionar mais credenciais aqui, juntamente no arquivo Json*/
-    var credenciais =  [
-        {
-            "email": "administrador@adm.com",
-            "senha": "Admin123"
+    try {
+        // Carregar credenciais do arquivo JSON
+        const response = await fetch('testelogin.json');
+        if (!response.ok) {
+            throw new Error('Erro ao carregar o arquivo JSON');
         }
-        ,
-    {
-        "email": "Pucminas@puc.br",
-        "senha": "Puc12345"
-    }
-    
-    ];
+        const credenciais = await response.json();
 
-    var loginValido = false;
-    for (var i = 0; i < credenciais.length; i++) {
-        if (credenciais[i].email === email && credenciais[i].senha === senha) {
-            loginValido = true;
-            break;
+        // Verificar as credenciais
+        var loginValido = false;
+        for (var i = 0; i < credenciais.length; i++) {
+            if (credenciais[i].email === email && credenciais[i].senha === senha) {
+                loginValido = true;
+                break;
+            }
         }
-    }
 
-    if (loginValido) {
-        alert('Login realizado com sucesso!');
-    } else {
-        alert('Email ou senha incorretos. Por favor, tente novamente.');
+        if (loginValido) {
+            window.location.href = '/HTML - Easy Mov/ADM - Andre/Logado.html';
+        } else {
+            alert('Email ou senha incorretos. Por favor, tente novamente.');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Ocorreu um erro ao tentar fazer login. Por favor, tente novamente mais tarde.');
     }
 }
+
+//CADASTRO DE BIKES
+
+    //configurar o ação do botão de cadastro
+    document.getElementById('cadastrarBike').addEventListener('click', cadastrarBike);
+
+    //ler bikes
+    async function leBikes(){
+        let strBikes = localStorage.getItem('Bikes')
+        let objBikes = {};
+        if (strBikes){
+            objBikes = JSON.parse (strDados);
+        }
+
+        return objBikes;
+    }
+
+    //ler bikes cadastradas
+    async function salvaBikes (dados){
+        localStorage.setItem('Bikes', JSON.stringify (dados));
+    }
+
+    //cadastrar bikes
+    async function cadastrarBike(){
+        //ler dados do localStorage
+        let objBikes = leBikes();
+
+        //cadastrar nova bike
+        let strImagem = document.getElementById ('imagemBike').value;
+        let strMarca = document.getElementById ('marcaBike').value;
+        let strAro = document.getElementById ('aroBike').value;
+        let strMarcha = document.getElementById ('marchaBike').value;
+        let strFreio = document.getElementById ('freiroBike').value;
+        let strCesta = document.getElementById ('cestaBike').value;
+        let strDescricao = document.getElementById ('descricaoBike').value;
+        let novaBike = {
+            Imagem: strImagem,
+            Marca: strMarca,
+            Aro: strAro,
+            Marcha: strMarcha,
+            Freio: strFreio,
+            Cesta: strCesta,
+            Descricao: strDescricao
+        };
+        objBikes.Bikes.push (novaBike);
+
+        //salva as bikes no localStorage novamente
+        salvaBikes (objBikes);
+
+        //informar o cadastro efetivado
+        alert('Bike cadastrada com sucesso');
+    }
