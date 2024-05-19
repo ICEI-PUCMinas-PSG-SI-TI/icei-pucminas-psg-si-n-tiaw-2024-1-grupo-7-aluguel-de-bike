dc52c6c6ad309fda67d9868971b15424c4c155b5
Davi <davi.avila@sga.pucminas.br>
2024-05-19 19:46:52 -0300

    Correção na página do javascript

--- HTML - Easy Mov/Cadastro Bike - Guedes/script.js
+++ HTML - Easy Mov/Cadastro Bike - Guedes/script.js
@@ -1,58 +1,22 @@
-//cadastro de clientes
-
-document.getElementById('loginButton').addEventListener('click', fazerLogin);
-
-async function fazerLogin() {
-    var email = document.getElementById('email').value;
-    var senha = document.getElementById('senha').value;
-
-    try {
-        // Carregar credenciais do arquivo JSON
-        const response = await fetch('testelogin.json');
-        if (!response.ok) {
-            throw new Error('Erro ao carregar o arquivo JSON');
-        }
-        const credenciais = await response.json();
-
-        // Verificar as credenciais
-        var loginValido = false;
-        for (var i = 0; i < credenciais.length; i++) {
-            if (credenciais[i].email === email && credenciais[i].senha === senha) {
-                loginValido = true;
-                break;
-            }
-        }
-
-        if (loginValido) {
-            window.location.href = '/HTML - Easy Mov/ADM - Andre/Logado.html';
-        } else {
-            alert('Email ou senha incorretos. Por favor, tente novamente.');
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-        alert('Ocorreu um erro ao tentar fazer login. Por favor, tente novamente mais tarde.');
-    }
-}
-
 //CADASTRO DE BIKES
 
     //configurar o ação do botão de cadastro
     document.getElementById('cadastrarBike').addEventListener('click', cadastrarBike);
 
     //ler bikes
-    async function leBikes(){
-        let strBikes = localStorage.getItem('Bikes')
+    function leBikes(){
+        let strBikes = localStorage.getItem('db')
         let objBikes = {};
         if (strBikes){
-            objBikes = JSON.parse (strDados);
+            objBikes = JSON.parse (strBikes);
         }
 
         return objBikes;
     }
 
     //ler bikes cadastradas
-    async function salvaBikes (dados){
-        localStorage.setItem('Bikes', JSON.stringify (dados));
+    function salvaBikes (dados){
+        localStorage.setItem('db', JSON.stringify (dados));
     }
 
     //cadastrar bikes
@@ -65,7 +29,7 @@ async function fazerLogin() {
         let strMarca = document.getElementById ('marcaBike').value;
         let strAro = document.getElementById ('aroBike').value;
         let strMarcha = document.getElementById ('marchaBike').value;
-        let strFreio = document.getElementById ('freiroBike').value;
+        let strFreio = document.getElementById ('freioBike').value;
         let strCesta = document.getElementById ('cestaBike').value;
         let strDescricao = document.getElementById ('descricaoBike').value;
         let novaBike = {
